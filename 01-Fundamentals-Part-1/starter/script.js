92b7f20437cb65051a009a586a4ecfa09b0aa8e0
GiftGiwa <gdg2131@columbia.edu>
2021-12-27 21:58:01 -0500

    progress

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,40 @@
+//9
+/*let js = 'amazing'
+if (js === 'amazing') alert('JavaScript is FUN!')
+40 + 8 + 23 - 10
+console.log(40 + 8 + 23 - 10)
+
+//Fundamentals pt1
+
+//values and variables
+let country = 'USA'
+let continent = 'North America'
+let population = 329500000
+console.log(country)
+console.log(continent)
+console.log(population)
+
+//data types 
+let isIsland = false
+let language
+console.log(isIsland)
+console.log(language)
+
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+
+console.log(typeof 56)*/
+
+//CODING CHALLENGE 1
+// BMI = mass / height ** 2 = mass / (height * height)
+const markM = 78
+const johnM = 92
+const markH = 1.69
+const johnH = 1.95
+
+const markBMI = markM / (markH ** 2)
+const johnBMI = johnM / (johnH ** 2)
+const markHigherBMI = markBMI > johnBMI
+
+console.log(markBMI, johnBMI, markHigherBMI)
+

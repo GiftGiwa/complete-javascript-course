cc1bc375e9f5d8fd08b96cd99fe8771bdbc3c70f
GiftGiwa <gdg2131@columbia.edu>
2022-01-02 23:29:19 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -50,9 +50,45 @@ if (markBMI > johnBMI) {
     console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`)
 } else {
     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})`)
-}*/
-
+}
 
 // it's considered a safe practice to solely rely on == instead of ===
 
+//CODING CHALLENGE 3
+let dolphins1 = 96, dolphins2 = 108, dolphins3 = 89
+let koalas1 = 88, koalas2 = 91, koalas3 = 110
+let dolphinsAvg = (dolphins1 + dolphins2 + dolphins3) / 3;
+let koalasAvg = (koalas1 + koalas2 + koalas3) / 3;
+console.log(dolphinsAvg, koalasAvg);
 
+/*if (dolphinsAvg > koalasAvg) {
+    console.log("Dolphins win!")
+} else if (dolphinsAvg === koalasAvg) {
+    console.log("Draw")
+} else {
+    console.log("Koalas win!")
+}
+
+if (dolphinsAvg > koalasAvg && dolphinsAvg >= 100) { //bonus 1
+    console.log('Dolphins win!')
+} else if (dolphinsAvg < koalasAvg && koalasAvg >= 100) {
+    console.log('Koalas win!')
+} else {
+    console.log('Draw')
+}
+
+if (dolphinsAvg > koalasAvg && dolphinsAvg >= 100) { //bonus 1
+    console.log('Dolphins win!')
+} else if (dolphinsAvg < koalasAvg && koalasAvg >= 100) {
+    console.log('Koalas win!')
+} else if (dolphinsAvg === koalasAvg && dolphinsAvg >= 100) {
+    console.log('Draw')
+} else {
+    console.log('No trophy')
+}
+
+//CODING CHALLENGE 4
+let bill = 430;
+let tip = ((bill >= 50 && bill <= 300) ? 0.15 : 0.20) * bill
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);*/

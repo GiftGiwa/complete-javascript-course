0bec15eaa44ce835f3fcec9d6dd88303812ee64d
GiftGiwa <gdg2131@columbia.edu>
2022-01-05 01:48:01 -0500

    Started section 3

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,28 @@
+'strict mode';
+/*
+// CODING CHALLENGE 1
+let dolphins1 = 44, dolphins2 = 23, dolphins3 = 71
+let koalas1 = 65, koalas2 = 54, koalas3 = 49
+
+//syntax for an anonymous arrow function
+let calcAverage = (score1, score2, score3) => {
+    return (score1 + score2 + score3) / 3
+}
+
+const dolphinsAvg = calcAverage(dolphins1, dolphins2, dolphins3)
+const koalasAvg = calcAverage(koalas1, koalas2, koalas3)
+// console.log(dolphinsAvg, koalasAvg)
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= avgKoalas * 2)
+        console.log(`Dolphins win (${dolphinsAvg} vs. ${koalasAvg})`)
+    else if (avgKoalas >= avgDolphins * 2)
+        console.log(`Koalas win (${koalasAvg} vs. ${dolphinsAvg})`)
+    else {
+        console.log(`No winners`)
+    }
+}
+
+checkWinner(dolphinsAvg, koalasAvg)
+*/
+
